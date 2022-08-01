97f6e98ac826378a095c112fcc80222bed99aea2
Egor Khamitsevich <lunexe@gmail.com>
2022-08-01 22:08:25 +0300

    feat: task compleate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  let rvr = n.toString().split("");
+  if (rvr.includes("-")) {
+    rvr.splice("-", 1);
+    
+  }
   
-}
+    return +rvr.reverse().join("");
+  };
+
